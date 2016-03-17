a91920102ea63097fc791ebea47329c1fe221be8
David Ostrovsky <david@ostrovsky.org>
2016-03-17 08:03:58 +0100

    WIP: Add verification info to the change screen
    Retreive verification info and visualize it on the change screen
    as a drop down panel.
    
    It's only rendered as a drop down now but later it can be additionally
    rendered aternatively as a panel on change screen directly, like the
    custom Javascript solution on review.o.o. And with a setting in user
    preferences, exposed by the plugin, user can switch between different
    rendering ways. There is also a option to switch the visualization
    of the verify informtaion off in this plugin and let custom plugin
    to use the data and visualize it in different ways.
    
    Change-Id: Id4980a7a5cf52271c5addfae25a33d49db958043

--- src/main/java/com/googlesource/gerrit/plugins/verifystatus/client/VerificationInfo.java
+++ src/main/java/com/googlesource/gerrit/plugins/verifystatus/client/VerificationInfo.java
@@ -1,13 +1,33 @@
 package com.googlesource.gerrit.plugins.verifystatus.client;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwtjsonrpc.client.impl.ser.JavaSqlTimestamp_JsonSerializer;
+
+import java.sql.Timestamp;
 
 public class VerificationInfo extends JavaScriptObject {
-
-  public final native String category() /*-{ return this.category; }-*/;
+  public static VerificationInfo create() {
+    return createObject().cast();
+  }
+  public final native String label() /*-{ return this.label; }-*/;
   public final native String url() /*-{ return this.url; }-*/;
-  public final native String granted() /*-{ return this.granted; }-*/;
-  public final native int value() /*-{ return this.value; }-*/;
+  public final native String comment() /*-{ return this.comment; }-*/;
+  public final native short value() /*-{ return this.value; }-*/;
+
+  public final Timestamp granted() {
+    Timestamp r = grantedTimestamp();
+    if (r == null) {
+      String s = grantedRaw();
+      if (s != null) {
+        r = JavaSqlTimestamp_JsonSerializer.parseTimestamp(s);
+        grantedTimestamp(r);
+      }
+    }
+    return r;
+  }
+  public final native String grantedRaw() /*-{ return this.granted; }-*/;
+  private final native Timestamp grantedTimestamp() /*-{ return this._ts }-*/;
+  private final native void grantedTimestamp(Timestamp t) /*-{ this._ts = t }-*/;
 
   protected VerificationInfo() {
   }
