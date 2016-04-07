b0960a8c6d451a79bc2fd48b80c762ae33ad66a3
Khai Do <zaro0508@gmail.com>
2016-04-07 13:20:08 -0700

    Refactor and add new data for jobs
    This change fixes stuff, refactors others and adds some new data columns.
    Basically this change is for backend improvements.
    
    This change does the following:
    1. Change `categoryId` to `jobId`
    2. Re-purpose `category` to represent a job category to help organize
    jobs.
    3. Changed `verifier` column to `reporter` since i feel the word
    `verify` and it's derivitives
    (verified, verification, verifier etc..) is overly used.
    4. Add the following columns:
      duration - the time for the job run
      abstain - whether pass/fail result should count as a vote
    5. Fix up some REST endpoint bugs
    6. Update docs to reflect this refactoring
    
    Change-Id: Ibbfb059bbca5144d67fd00143e9a7846eed442af

--- src/main/java/com/googlesource/gerrit/plugins/verifystatus/server/PostVerification.java
+++ src/main/java/com/googlesource/gerrit/plugins/verifystatus/server/PostVerification.java
@@ -96,19 +96,29 @@ public class PostVerification
       if (c != null) {
         c.setGranted(ts);
         c.setValue(value);
+        Boolean abstain = ent.getValue().abstain;
+        c.setAbstain(abstain);
         String url = ent.getValue().url;
         if (url != null) {
           c.setUrl(url);
         }
-        String verifier = ent.getValue().verifier;
-        if (verifier != null) {
-          c.setVerifier(verifier);
+        String reporter = ent.getValue().reporter;
+        if (reporter != null) {
+          c.setReporter(reporter);
         }
         String comment = ent.getValue().comment;
         if (comment != null) {
           c.setComment(comment);
         }
-        log.info("Updating job " + c.getLabel() + " for change "
+        String category = ent.getValue().category;
+        if (category != null) {
+          c.setCategory(category);
+        }
+        String duration = ent.getValue().duration;
+        if (duration != null) {
+          c.setDuration(duration);
+        }
+        log.info("Updating job " + c.getJob() + " for change "
             + c.getPatchSetId());
         ups.add(c);
       } else {
@@ -116,11 +126,13 @@ public class PostVerification
                 resource.getPatchSet().getId(),
                 new LabelId(name)),
             value, TimeUtil.nowTs());
-        c.setGranted(ts);
+        c.setAbstain(ent.getValue().abstain);
         c.setUrl(ent.getValue().url);
-        c.setVerifier(ent.getValue().verifier);
+        c.setReporter(ent.getValue().reporter);
         c.setComment(ent.getValue().comment);
-        log.info("Adding job " + c.getLabel() + " for change "
+        c.setCategory(ent.getValue().category);
+        c.setDuration(ent.getValue().duration);
+        log.info("Adding job " + c.getJob() + " for change "
             + c.getPatchSetId());
         ups.add(c);
       }
@@ -136,7 +148,7 @@ public class PostVerification
     Map<String, PatchSetVerification> current = Maps.newHashMap();
     for (PatchSetVerification v : db.patchSetVerifications()
         .byPatchSet(resource.getPatchSet().getId())) {
-      current.put(v.getLabelId().get(), v);
+      current.put(v.getJobId().get(), v);
     }
     return current;
   }
