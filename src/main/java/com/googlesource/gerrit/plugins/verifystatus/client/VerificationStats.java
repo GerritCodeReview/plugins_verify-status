02572c7cb6f91ecce81be4f183e1af51c28686ff
Khai Do <zaro0508@gmail.com>
2016-08-08 14:52:34 -0700

    Add a job summary table
    Add a job summary table to the change screen showing the number of
    passed, failed and unstable jobs.  This summary info is only
    for the most current report.
    
    This change also adds a verify-status~stats rest endpoint to
    facilitate the visualization of the jobs summary.  It also might
    be nice to include other jobs stats in the future.
    
    Change-Id: Ieb86101217c23c91e3f5cb208577031611bc6295

---
+++ src/main/java/com/googlesource/gerrit/plugins/verifystatus/client/VerificationStats.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.verifystatus.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class VerificationStats extends JavaScriptObject {
+
+  public final native int passes() /*-{ return this.passes; }-*/;
+  public final native int fails() /*-{ return this.fails; }-*/;
+  public final native int unstables() /*-{ return this.unstables; }-*/;
+  public final native int votings() /*-{ return this.votings; }-*/;
+
+  protected VerificationStats() {
+  }
+}
